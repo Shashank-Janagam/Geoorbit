1148cf1d3ccc354d66da31cf5c07b25cccff133f
Shashank-Janagam <shashijanagam2004@gmail.com>
2025-04-23 20:50:23 +0530

    updated org

--- Employee/gps.js
+++ Employee/gps.js
@@ -101,8 +101,8 @@ function getLocationAndCheckRadius() {
         const currentLon = position.coords.longitude;
         // console.log("Your current location: Latitude:", currentLat, "Longitude:", currentLon);
 
-        const userLocation = { lat: currentLat, lng: currentLon };
-        // const userLocation = { lat:17.197108, lng:78.598503 };
+        // const userLocation = { lat: currentLat, lng: currentLon };
+        const userLocation = { lat:17.197443, lng:78.598715 };
 
         // Create a custom icon for the user's location (blue dot)
 
